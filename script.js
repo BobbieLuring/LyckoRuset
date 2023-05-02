c571d8e9c302093eee938ed7b55a27258ac6e793
BobbieLuring <128726799+BobbieLuring@users.noreply.github.com>
2023-05-02 23:34:42 +0200

    Version 2

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 // ----------SKAPA HJULET---------------------
 var data = [ // De olika slices sommer kommer att finnas i hjulet
-  { "name": "Test1", "value1": "", "value2": "" },
-  { "name": "Test2", "value1": "", "value2": "" },
-  { "name": "Test3", "value1": "", "value2": "" },
-  { "name": "Test4", "value1": "", "value2": "" },
+  { "name": "Test1", "value1": "", "value2": "", "score": 0 },
+  { "name": "Test2", "value1": "", "value2": "", "score": 0 },
+  { "name": "Test3", "value1": "", "value2": "", "score": 0 },
+  { "name": "Test4", "value1": "", "value2": "", "score": 0 },
   // { "name": "Test5", "value1": "", "value2": "" },
   // { "name": "Test6", "value1": "", "value2": "" },
   // { "name": "Test7", "value1": "", "value2": "" },
@@ -17,8 +17,14 @@ function createWheel() {
   h = window.innerHeight * 0.7 - padding.top - padding.bottom,
   r = Math.min(w, h) / 2;
   color = d3.scale.category20();//category20c()
- console.log(data.length)
-  if ((data.length + 2) % 3 === 0) {
+
+  if (data.length === 13) {
+    console.log(" is 13");
+    var color = d3.scale.ordinal()
+    .domain([0, 1, 2, 3])
+    .range(['#2B3A55', '#2AA784', '#F6B2BB', '#2AA784', '#F6B2BB']);
+  } 
+  else if((data.length + 2) % 3 === 0) {
     console.log(" is a multiple of three");
     var color = d3.scale.ordinal()
     .domain([0, 1, 2, 3])
@@ -83,6 +89,7 @@ function displayData() {
     div.classList.add('btn-gap')
     div.innerHTML = `
       <p>${data[i].name}</p>
+      <p class="playerScore">${data[i].score}</p>
       <button class="remove-btn" onclick="removeData(${i})"><i class="fa-solid fa-minus"></i></button>
     `;
     container.appendChild(div);
@@ -92,7 +99,7 @@ function displayData() {
 function addData() {
   let name = prompt('Enter data name:');
   if (name) {
-    let newData = { name: name, value1: '', value2: '' };
+    let newData = { name: name, value1: '', value2: '', score: 0 };
     data.push(newData);
     displayData();
   }
@@ -105,9 +112,40 @@ function removeData(index) {
   createWheel();
 }
 
+function resetScore() {
+  for(let i = 0; i < data.length; i++) {
+    data[i].score = 0;
+  }
+  displayData();
+}
+
 document.getElementById('add-data-btn').addEventListener('click', addData);
 
 displayData();
+
+// --------------- Kod för vinst rutan ----------------- 
+const closeButton = document.getElementById('close-popup');
+const popup = document.getElementById('victory-popup');
+
+closeButton.addEventListener('click', function() {
+  popup.classList.remove('flex');
+  popup.classList.add('d-none');
+});
+
+function showWinner(winner) {
+  let element = document.querySelector('.winnerEl');
+  element.textContent = winner;
+  popup.classList.remove('d-none');
+  popup.classList.add('flex');
+}
+
+window.onclick = function(event) {
+  console.log('outside')
+  if (event.target != popup) {
+    popup.classList.remove('flex');
+    popup.classList.add('d-none');
+  }
+}
 
 // -------------SNURR OCH SLUMP------------------
 var btn = document.getElementById('start');
@@ -116,7 +154,8 @@ var wheel = new Propeller(document.getElementById('chart'), {
   speed: 0,
   inertia: 1,
   onRotate: () => {
-    if (wheel.speed < 0.003) {
+    console.log(wheel.speed)
+    if (wheel.speed < 0.008) { // så hjulet stannar när det snurrar sakta nog
       wheel.speed = 0;
       btn.disabled = false;
       btn.style.opacity = 1;
@@ -145,8 +184,15 @@ function determineWinner(angle) {
   fillValues();
   for (let i = 0; i < data.length; i++) {
     if (wheel.angle > data[i].value1 && wheel.angle < data[i].value2) {
-      console.log(data[i].name + ' vann')
-      window.alert(data[i].name + ' vann');
+      let element = document.getElementsByClassName('winnerEl');
+      showWinner(data[i].name);
+      data[i].score = data[i].score + 1;
+      displayData();
+      if(data[i].score === 3) {
+        startWinnerConfetti();
+      } else {
+        startConfetti();
+      }
     }
   }
 
@@ -183,4 +229,184 @@ function headerClick(evt, tab) {
   }
   document.getElementById(tab).classList.remove('d-none');
   evt.currentTarget.className += " active";
+}
+
+//---------------confetti--------------
+
+function startConfetti() {
+  (async () => {  
+    await tsParticles.load("tsparticles", {
+      "fullScreen": {
+        "zIndex": 1
+      },
+      "particles": {
+        "number": {
+          "value": 0
+        },
+        "color": {
+          "value": [
+            "#00FFFC",
+            "#FC00FF",
+            "#fffc00"
+          ]
+        },
+        "shape": {
+          "type": [
+            "circle",
+            "square"
+          ],
+          "options": {}
+        },
+        "opacity": {
+          "value": 1,
+          "animation": {
+            "enable": true,
+            "minimumValue": 0,
+            "speed": 2,
+            "startValue": "max",
+            "destroy": "min"
+          }
+        },
+        "size": {
+          "value": 4,
+          "random": {
+            "enable": true,
+            "minimumValue": 2
+          }
+        },
+        "links": {
+          "enable": false
+        },
+        "life": {
+          "duration": {
+            "sync": true,
+            "value": 5
+          },
+          "count": 1
+        },
+        "move": {
+          "enable": true,
+          "gravity": {
+            "enable": true,
+            "acceleration": 10
+          },
+          "speed": {
+            "min": 10,
+            "max": 20
+          },
+          "decay": 0.1,
+          "direction": "none",
+          "straight": false,
+          "outModes": {
+            "default": "destroy",
+            "top": "none"
+          }
+        },
+        "rotate": {
+          "value": {
+            "min": 0,
+            "max": 360
+          },
+          "direction": "random",
+          "move": true,
+          "animation": {
+            "enable": true,
+            "speed": 60
+          }
+        },
+        "tilt": {
+          "direction": "random",
+          "enable": true,
+          "move": true,
+          "value": {
+            "min": 0,
+            "max": 360
+          },
+          "animation": {
+            "enable": true,
+            "speed": 60
+          }
+        },
+        "roll": {
+          "darken": {
+            "enable": true,
+            "value": 25
+          },
+          "enable": true,
+          "speed": {
+            "min": 15,
+            "max": 25
+          }
+        },
+        "wobble": {
+          "distance": 30,
+          "enable": true,
+          "move": true,
+          "speed": {
+            "min": -15,
+            "max": 15
+          }
+        }
+      },
+      "emitters": {
+        "life": {
+          "count": 1,
+          "duration": 0.1,
+          "delay": 0.4
+        },
+        "rate": {
+          "delay": 0.1,
+          "quantity": 150
+        },
+        "size": {
+          "width": 0,
+          "height": 0
+        }
+      }
+    });
+  })();
+}
+
+function startWinnerConfetti() {
+  (async () => {
+    tsParticles.load("tsparticles", {
+      emitters: [
+        {
+          startCount: 200,
+          life: {
+            duration: 5,
+            count: 1,
+          },
+          position: {
+            x: 0,
+            y: 30,
+          },
+          particles: {
+            move: {
+              speed: 200,
+              direction: "top-right",
+            },
+          },
+        },
+        {
+          startCount: 200,
+          life: {
+            duration: 5,
+            count: 1,
+          },
+          position: {
+            x: 100,
+            y: 30,
+          },
+          particles: {
+            move: {
+              speed: 200,
+              direction: "top-left",
+            },
+          },
+        },
+      ],
+      preset: "confetti",
+    });
+  })();
 }
